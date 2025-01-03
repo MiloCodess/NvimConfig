27a260e529882b1600424257016590466a336fa9
Milo Arjana <Milokeanuarjana@icloud.com>
2025-01-03 13:03:06 -0600

    changed entire file setup for readability

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- ===========================================
+-- Bootstrap mini.nvim
+-- ===========================================
 local path_package = vim.fn.stdpath("data") .. "/site/"
 local mini_path = path_package .. "pack/deps/start/mini.nvim"
 
@@ -15,11 +18,12 @@ if not vim.loop.fs_stat(mini_path) then
 	vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
 
--- Set up mini.deps
+-- ===========================================
+-- Setup mini.nvim
+-- ===========================================
 require("mini.deps").setup({ path = { package = path_package } })
 local add = MiniDeps.add
 
--- Add `mini.files` properly
 add({
 	source = "echasnovski/mini.nvim",
 	name = "files", -- Required for mini.files
@@ -27,22 +31,15 @@ add({
 
 require("mini.files").setup()
 require("mini.ai").setup()
--- Initialize packer and add plugins
---
 
+-- ===========================================
+-- Packer Plugin Management
+-- ===========================================
 require("packer").startup(function(use)
 	use("rcarriga/nvim-notify")
-	config = function()
-		require("notify").setup({
-			stages = "fade_in_slide_out",
-			background_colour = "FloatShadow",
-			timeout = 3000,
-		})
-	end
 	use({
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
-		-- or                            , branch = '0.1.x',
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 	use("nvim-lua/plenary.nvim")
@@ -61,16 +58,14 @@ require("packer").startup(function(use)
 			require("toggleterm").setup()
 		end,
 	})
-
 	use({
 		"kylechui/nvim-surround",
-		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		tag = "*",
 		config = function()
 			require("nvim-surround").setup()
 		end,
 	})
 	use("tpope/vim-sleuth")
-
 	use({
 		"goolord/alpha-nvim",
 		config = function()
@@ -80,8 +75,7 @@ require("packer").startup(function(use)
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-			ts_update()
+			require("nvim-treesitter.install").update({ with_sync = true })
 		end,
 	})
 	use({
@@ -91,109 +85,93 @@ require("packer").startup(function(use)
 		end,
 	})
 end)
+
+-- ===========================================
+-- Plugin Configurations
+-- ===========================================
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "c", "python", "javascript", "lua" }, -- Add your languages here
-	highlight = {
-		enable = true, -- Enable Tree-sitter highlighting
-		additional_vim_regex_highlighting = false, -- Disable default regex-based highlighting
-	},
+	ensure_installed = { "c", "python", "javascript", "lua" },
+	highlight = { enable = true, additional_vim_regex_highlighting = false },
 })
+
 require("noice").setup({
 	lsp = {
-		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 		override = {
 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+			["cmp.entry.get_documentation"] = true,
 		},
 	},
-	-- you can enable a preset for easier configuration
 	presets = {
-		bottom_search = true, -- use a classic bottom cmdline for search
-		command_palette = true, -- position the cmdline and popupmenu together
-		long_message_to_split = true, -- long messages will be sent to a split
-		inc_rename = false, -- enables an input dialog for inc-rename.nvim
-		lsp_doc_border = false, -- add a border to hover docs and signature help
+		bottom_search = true,
+		command_palette = true,
+		long_message_to_split = true,
+		inc_rename = false,
+		lsp_doc_border = false,
 	},
 })
-require("transparent").setup({
-	enable = true, -- boolean: enable transparent
-	extra_groups = { -- table/string: additional groups that should be clear
-		-- In this case, we need to specify `cterm` as well
-		-- as `gui` to ensure that it's cleared on all terminals
-		-- this is the default
-		-- "GitGutter",
-		-- "LspDiagnosticsDefaultError",
-		-- "LspDiagnosticsDefaultWarning",
-		-- "LspDiagnosticsDefaultInformation",
-		-- "LspDiagnosticsDefaultHint"
-	},
-	exclude = {}, -- table: groups you don't want to clear
-})
-require("ibl").setup()
+
+require("transparent").setup({ enable = true, extra_groups = {}, exclude = {} })
+
 require("smart-splits").setup({
-	-- You can customize these options as needed.
-	default_amount = 3, -- Number of lines/columns to resize by at a time.
-	at_edge = "wrap", -- Behavior when at the edge of a split.
+	default_amount = 3,
+	at_edge = "wrap",
 })
+
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
-		-- Conform will run multiple formatters sequentially
 		python = { "isort", "black" },
-		-- You can customize some of the format options for the filetype (:help conform.format)
 		rust = { "rustfmt", lsp_format = "fallback" },
-		-- Conform will run the first available formatter
 		javascript = { "prettierd", "prettier", stop_after_first = true },
 	},
 })
--- Resize splits
+
+-- ===========================================
+-- Keybindings
+-- ===========================================
 vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
 vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
 vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
 vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
 
--- Moving between splits
 vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
 vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
 vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
 vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
 
-vim.g.mapleader = " " -- Set space as the leader key
-vim.api.nvim_set_keymap("n", "<Leader>f", ":lua MiniFiles.open()<CR>", { noremap = true, silent = true })
-
-vim.opt.termguicolors = true
-
-vim.api.nvim_set_keymap(
+vim.keymap.set("n", "<Leader>f", ":lua MiniFiles.open()<CR>", { noremap = true, silent = true })
+vim.keymap.set(
 	"n",
 	"<space>g",
 	":ToggleTerm size=40 dir=~/Desktop direction=float name=desktop<CR> | startinsert",
 	{ noremap = true, silent = true }
 )
-require("telescope").load_extension("fzf")
-
-require("telescope").load_extension("live_grep_args")
-require("telescope").load_extension("noice")
-
-require("notify")("My super important message")
-
-vim.notify = require("notify")
-
--- Set custom Tree-sitter highlight colors
-vim.api.nvim_set_hl(0, "TSFunction", { fg = "#EE8B8E", bold = true }) -- Functions in gold
-vim.api.nvim_set_hl(0, "TSKeyword", { fg = "#D8627C", italic = true }) -- Keywords in orange
-vim.api.nvim_set_hl(0, "TSString", { fg = "#8DD0D9" }) -- Strings in green
-vim.api.nvim_set_hl(0, "TSComment", { fg = "#AF95B0", italic = true }) -- Comments in gray
-vim.api.nvim_set_hl(0, "TSVariable", { fg = "#39BFD8" }) -- Variables in light blue
-vim.api.nvim_set_hl(0, "TSConstant", { fg = "#E7B59E" }) -- Constants in tomato red
-
-vim.api.nvim_set_keymap("n", "<Leader>f", ":lua MiniFiles.open()<CR>", { noremap = true, silent = true })
-
 vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>")
 
+-- ===========================================
+-- Additional Settings
+-- ===========================================
+vim.g.mapleader = " "
+vim.opt.termguicolors = true
+vim.notify = require("notify")
+
+-- ===========================================
+-- Autocommands
+-- ===========================================
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*",
 	callback = function(args)
 		require("conform").format({ bufnr = args.buf })
 	end,
 })
+
+-- ===========================================
+-- Custom Highlights
+-- ===========================================
+vim.api.nvim_set_hl(0, "TSFunction", { fg = "#EE8B8E", bold = true })
+vim.api.nvim_set_hl(0, "TSKeyword", { fg = "#D8627C", italic = true })
+vim.api.nvim_set_hl(0, "TSString", { fg = "#8DD0D9" })
+vim.api.nvim_set_hl(0, "TSComment", { fg = "#AF95B0", italic = true })
+vim.api.nvim_set_hl(0, "TSVariable", { fg = "#39BFD8" })
+vim.api.nvim_set_hl(0, "TSConstant", { fg = "#E7B59E" })
